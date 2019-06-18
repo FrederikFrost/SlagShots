5062d4f311c7560520a12966cc862cf41f05b355
Elias Ismail <eliasdk@yahoo.dk>
2019-06-18 15:56:52 +0200

    Tilføjet username dialog + lidt diverse

--- app/src/main/java/com/example/bruger/slagshots/HomeActivity.java
+++ app/src/main/java/com/example/bruger/slagshots/HomeActivity.java
@@ -1,17 +1,23 @@
 package com.example.bruger.slagshots;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+    private String inputName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedPreferences = getSharedPreferences("prefs",MODE_PRIVATE);
+        showStartDialog();
 
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +54,23 @@ public class HomeActivity extends AppCompatActivity {
 
 
     }
+    private void showStartDialog(){
+        final EditText editText = new EditText(HomeActivity.this);
+        new AlertDialog.Builder(this)
+                .setTitle("SlagShots!")
+                .setMessage('\n' + "Indtast dit navn")
+                .setView(editText)
+                .setIcon(R.mipmap.ic_launcher)
+                .setPositiveButton("Bekræft", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        inputName = editText.getText().toString();
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
 
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -71,6 +95,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private void opretSpil(){
       Intent intent = new Intent(HomeActivity.this,NewGameActivity.class);
+        intent.putExtra("inputName",inputName);
       startActivity(intent);
     }
 
