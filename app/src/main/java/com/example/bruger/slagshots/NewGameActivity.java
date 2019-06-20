a9af12954e9ef6fa84a5611986059614e3149a0c
Frederik Frost <s184182@student.dtu.dk>
2019-06-20 12:48:39 +0200

    Opdatering af Android Studio (3.4.1), og Gradle (5.1.1), SDK (28).

--- app/src/main/java/com/example/bruger/slagshots/NewGameActivity.java
+++ app/src/main/java/com/example/bruger/slagshots/NewGameActivity.java
@@ -12,7 +12,15 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class NewGameActivity extends AppCompatActivity {
+    private FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,31 @@ public class NewGameActivity extends AppCompatActivity {
         String inputName = intent.getStringExtra("inputName");
         TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setText("1.      " + inputName + '\n' + "2.      " + "Frederik");
+
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = mDatabase.getReference("message");
+
+        mRef.setValue("Hello, World");
+        int counter;
+
+        // Get a reference to our posts
+        DatabaseReference counterRef = mDatabase.getReference("GameRoomCounter");
+
+        // Attach a listener to read the data at our posts reference
+        counterRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String counter = dataSnapshot.getValue().toString();
+                int counterInt = Integer.parseInt(counter);
+                counter = String.format("%06d", counterInt);
+                System.out.println(counter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
 
 
     }
